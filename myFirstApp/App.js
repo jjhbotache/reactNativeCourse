5dce5bd3c4e63436da1f5c7509866e4f401523a9
jjhbotache <jjhuertasbotache@gmail.com>
2023-01-24 12:06:13 -0500

    day 2

--- myFirstApp/App.js
+++ myFirstApp/App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
+  const [user,setUser] = useState("Juanitojose")
+  const pressHandler = ()=>{
+    setUser("Juan")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.myStyle}>{user} esta aprendiendo</Text>
+      <Text>de a pocos :D</Text>
+
+      <View style={styles.myButton}>
+        <Button title='hola'
+        onPress={pressHandler}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  myStyle:{
+    color:"grey",
+    fontWeight:'bold',
+    fontSize:50,
+    textAlign:'center'
+    
+  },
+  myButton:{
+    color:"black",
+    textAlign:'right',
+    marginTop:50
+  }
+
 });
